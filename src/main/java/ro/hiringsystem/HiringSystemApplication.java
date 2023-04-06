c43af36e5584efbfdbe6918ab3384f50787385b3
DragosGhinea <dragos.dev12@gmail.com>
2023-04-06 23:57:28 +0300

    Loading Spring Boot layout

---
+++ src/main/java/ro/hiringsystem/HiringSystemApplication.java
@@ -0,0 +1,13 @@
+package ro.hiringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/hiringsystem/HiringSystem/HiringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.hiringsystem.HiringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
