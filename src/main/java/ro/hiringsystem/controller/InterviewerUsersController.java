64025fd82fcc1cca1d06112a5bf0cc387f3c29e1
stefaniarincu <93484228+stefaniarincu@users.noreply.github.com>
2023-06-06 12:59:33 +0300

    Added manual user creation system

--- src/main/java/ro/hiringsystem/controller/InterviewerUsersController.java
+++ src/main/java/ro/hiringsystem/controller/InterviewerUsersController.java
@@ -1,39 +1,39 @@
 package ro.hiringsystem.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.hiringsystem.model.dto.InterviewerUserDto;
-import ro.hiringsystem.model.entity.InterviewerUser;
 import ro.hiringsystem.model.enums.InterviewerType;
+import ro.hiringsystem.service.InterviewerUserService;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1/interviewer")
+@RequiredArgsConstructor
 public class InterviewerUsersController {
+    private final InterviewerUserService interviewerUserService;
 
-    @GetMapping("profile")
-    public ResponseEntity<InterviewerUserDto> getInterviewerUser() {
-
-        return ResponseEntity.ok(InterviewerUserDto.builder()
-                .id(UUID.randomUUID())
-                .firstName("Jane")
-                .lastName("Bergshire")
-                .primaryEmail("janeberg@gmail.com")
-                .password("test")
-                .mailList(List.of("janebergshire@yahoo.com", "jane.berghsire@gmail.com"))
-                .phoneNumberList(List.of("0724135235", "0735647578"))
-                .birthDate(LocalDate.of(1980, 9, 11))
-                .interviewerType(InterviewerType.TECHNICAL)
-                .professionalBackground("Jane is a software developer with 10 years of experience in web development using languages such as JavaScript, Python, and PHP. She has expertise in front-end development with React and Angular and has worked on enterprise-level applications. She's proficient in agile methodologies and version control tools like Git, with experience in continuous integration and deployment tools. Jane is a strong problem-solver and effective communicator with a passion for learning and keeping up-to-date with the latest technologies.")
-                .build());
-
+    @GetMapping("profile/{id}")
+    public ResponseEntity<InterviewerUserDto> getInterviewerUser(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(interviewerUserService.getById(id));
     }
 
+    @PostMapping("create")
+    public ResponseEntity<InterviewerUserDto> createInterviewerUser(@RequestBody InterviewerUserDto interviewerUserDto){
+        return ResponseEntity.ok(interviewerUserService.create(interviewerUserDto));
+    }
+
+    @GetMapping("/types")
+    public ResponseEntity<InterviewerType[]> getAllInterviewerTypes() {
+        InterviewerType[] selectedTypes = Arrays.stream(InterviewerType.values())
+                .filter(type -> type != InterviewerType.NONE)
+                .toArray(InterviewerType[]::new);
+
+        return ResponseEntity.ok(selectedTypes);
+    }
 }
