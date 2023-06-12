5813bfe44a6b0bbecb4aa4bbb793593cc7b787cd
stefaniarincu <93484228+stefaniarincu@users.noreply.github.com>
2023-06-12 22:31:44 +0300

    Added the display of CV modal

--- src/main/java/ro/hiringsystem/mapper/ProjectMapper.java
+++ src/main/java/ro/hiringsystem/mapper/ProjectMapper.java
@@ -1,8 +1,10 @@
 package ro.hiringsystem.mapper;
 
+import org.mapstruct.Mapper;
 import ro.hiringsystem.model.auxiliary.Project;
 import ro.hiringsystem.model.dto.cv.ProjectDto;
 
+@Mapper
 public interface ProjectMapper {
 
     ProjectDto toDto(Project project);
